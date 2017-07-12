6fb8c683ab907f62316a9af5aadd78173d67c9cd
Bulat Giniyatullin <laineg2014@gmail.com>
2017-07-12 16:23:25 +0300

    empty views added

--- KPITest/views.py
+++ KPITest/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request, user_id):
+    pass
+
+
+def stats(request, user_id):
+    pass
+
+
+def employees(request):
+    pass
+
+
+def tasks(request, user_id):
+    pass
+
+
+def employees_tasks(request):
+    pass
