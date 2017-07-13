82d4e2a2fbcb84edc980fef1cb3f68785fd52bdc
Рустем Саитгареев <srustem3@yandex.ru>
2017-07-13 13:00:56 +0300

    Во views.py добавлены методы tasks и employees_tasks + небольшие исправления в моделях (убрана лишняя связь department у Employee)

--- KPITest/views.py
+++ KPITest/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db.models import QuerySet
+from django.shortcuts import render, get_object_or_404
+
+from KPITest.models import Employee, Department, Task
 
 
 def profile(request, user_id):
@@ -13,9 +18,25 @@ def employees(request):
     pass
 
 
+@login_required(login_url='/')
 def tasks(request, user_id):
-    pass
+    user = get_object_or_404(User, pk=user_id)
+    employee = get_object_or_404(Employee, user=user)
+    context = {
+        'tasks': Employee.view_my_tasks(employee)
+    }
+    return render(request, 'personal_tasks.html', context)  # Прописать правильно
 
 
+@login_required(login_url='/')
 def employees_tasks(request):
-    pass
+    user = get_object_or_404(User, pk=request.user)
+    director = get_object_or_404(Employee, user=user)
+    departments = Department.objects.filter(directors__contains=director)
+    employees = departments.employees.distinct()
+    tasks = Task.objects.filter(owner__in=employees)
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'employees_tasks.html', context)  # Прописать правильно
+
