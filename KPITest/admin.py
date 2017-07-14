a31fad503e77652b8004c8e10d784009cd27dd09
Bulat Giniyatullin <laineg2014@gmail.com>
2017-07-14 09:46:26 +0300

    зарегистрированы модели департамента и задания

--- KPITest/admin.py
+++ KPITest/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from KPITest.models import Employee, Position
+from KPITest.models import Employee, Position, Department, Task
 
 admin.site.register(Employee)
 admin.site.register(Position)
+admin.site.register(Department)
+admin.site.register(Task)
