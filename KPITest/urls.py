21e2f3eb5752e3cfac9057898077882b47c1a72b
Bulat Giniyatullin <laineg2014@gmail.com>
2017-07-18 01:20:38 +0300

    не работающая url закомментирована

--- KPITest/urls.py
+++ KPITest/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
     url(r'^stats/report/(?P<report_id>[0-9]+)$', views.report, name='report'),
     url(r'^stats/reports/(?P<task_id>[0-9]+)$', views.reports_list, name='reports_list'),
     url(r'^tasks/$', views.tasks, name='tasks'),
-    #url(r'^tasks/update/(?P<task_id>[0-9]+)$', views.update_task, name='update_task'),
+    url(r'^tasks/update/(?P<task_id>[0-9]+)$', views.update_task, name='update_task'),
     url(r'^tasks/create$', views.create_task, name='create_task'),
-    url(r'^tasks/execute/(?P<task_id>[0-9]+)$', views.execute_task, name='execute_task'),
+    # url(r'^tasks/execute/(?P<task_id>[0-9]+)$', views.execute_task, name='execute_task'),
     url(r'^tasks/employees/$', views.employees_tasks, name='employees_tasks'),
     url(r'^$', views.redirect_to_login_page, name='redirect_to_login_page'),
 ]
